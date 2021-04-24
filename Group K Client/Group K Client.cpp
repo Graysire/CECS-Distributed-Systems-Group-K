ae60302727cf1f5086d720de8d084be921d9c8f0
Graysire <Graysire@dyrix.net>
2021-04-23 19:39:19 -0700

    Added Client and Server code
    Added basic client and server code, assuming the server is run first, the projects do connect to each other.

--- Group K Client/Group K Client.cpp
+++ Group K Client/Group K Client.cpp
@@ -1,20 +1,53 @@
 // Group K Client.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
 
+
 #include <iostream>
+#include <boost/asio.hpp>
+
+using namespace boost;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    //https://subscription.packtpub.com/book/application_development/9781783986545/1/ch01lvl1sec15/connecting-a-socket
+
+    // Step 1. Assume that the client application has already
+    // obtained the IP address and protocol port number of the
+    // target server.
+    std::string targetRawIP = "127.0.0.1";
+    unsigned short targetPort = 3333;
+
+    try {
+        // Step 2. Creating an endpoint designating  target server application.
+        // this will be used by the sockets as an access point for communication using TCP
+        asio::ip::tcp::endpoint clientEndpoint(asio::ip::address::from_string(targetRawIP), targetPort);
+
+        // I/O context that is used as a basis for communicating I/O commands to the OS
+        asio::io_context ios;
+
+        // Step 3. Creating and opening a socket.
+        // this will allow the socket to be used
+        asio::ip::tcp::socket socket(ios, clientEndpoint.protocol());
+
+        // Step 4. Connecting a socket.
+        // this will attemptto connect the socket to the server
+        socket.connect(clientEndpoint);
+
+        std::cout << "connected" << std::endl;
+
+        // At this point socket 'sock' is connected to 
+        // the server application and can be used
+        // to send data to or receive data from it.
+    }
+    // Overloads of asio::ip::address::from_string() and 
+    // asio::ip::tcp::socket::connect() used here throw
+    // exceptions in case of error condition.
+    catch (system::system_error& e) {
+        std::cout << "Error occured! Error code = " << e.code()
+            << ". Message: " << e.what();
+
+        return e.code().value();
+    }
+
+    return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
--- Group K Server/Group K Server.cpp
+++ Group K Server/Group K Server.cpp
@@ -1,20 +1,62 @@
 // Group K Server.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
 
+
 #include <iostream>
+#include <boost/asio.hpp>
+
+using namespace boost;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    //https://subscription.packtpub.com/book/application_development/9781783986545/1/ch01lvl1sec16/accepting-connections
+
+    // The size of the queue containing the pending connection requests.
+    const int BACKLOG_SIZE = 30;
+
+    //this is the prot that will be listened to
+    unsigned short listenPort = 3333;
+
+    // Creates the endpoint for use by the socket(s) for communication using TCP
+    asio::ip::tcp::endpoint serverEndpoint(asio::ip::address_v4::any(), listenPort);
+
+    // I/O context that is used as a basis for communicating I/O commands to the OS
+    asio::io_context ios;
+
+    //Attempt to create the connection
+    try {
+        // Step 3. Instantiating and opening an acceptor socket.
+        //this will be used to listen for incoming connections
+        asio::ip::tcp::acceptor acceptor(ios, serverEndpoint.protocol());
+
+        // Step 4. Binding the acceptor socket to the server endpoint
+        // Connect the acceptorand the endpoint to allow the acceptor to listen for connection requests
+        acceptor.bind(serverEndpoint);
+
+        // Step 5. Starting to listen for incoming connection requests
+        //Listen for any incoming requests with a maximum queue of BACKLOG_SIZE
+        acceptor.listen(BACKLOG_SIZE);
+
+        // Step 6. Creating an active socket.
+        //Create an active socket to connect to any communication requests received
+        asio::ip::tcp::socket socket(ios);
+
+        // Step 7. Processing the next connection request and connecting the active socket to the client.
+        //block until we receive a request, then connect the socket ot the request
+        acceptor.accept(socket);
+
+        std::cout << "accepted" << std::endl;
+
+        // At this point 'sock' socket is connected to 
+        // the client application and can be used to send data to
+        // or receive data from it.
+    }
+    catch (system::system_error& e) {
+        std::cout << "Error occured! Error code = " << e.code()
+            << ". Message: " << e.what();
+
+        return e.code().value();
+    }
+
+    return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
