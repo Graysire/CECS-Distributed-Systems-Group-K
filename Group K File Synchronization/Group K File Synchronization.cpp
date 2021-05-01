409ab88e39ca2e10d98eda07e89ba762018d8a28
Graysire <Graysire@dyrix.net>
2021-04-30 18:21:44 -0700

    Added find IP to main program
    Added find IP functions to the main program, it can successfully connect to the server program using an external IP (between separate machines untested)

--- Group K File Synchronization/Group K File Synchronization.cpp
+++ Group K File Synchronization/Group K File Synchronization.cpp
@@ -2,11 +2,14 @@
 //
 
 #include <iostream>
+#include <vector>
 #include <boost/asio.hpp>
 #include <boost/array.hpp>
 
 using namespace boost;
 using boost::asio::ip::tcp;
+
+const unsigned short PORT = 3333;
 
 //writes message to a socket synchronously
 void writeToSocket(asio::ip::tcp::socket& socket, std::string message)
@@ -19,104 +22,108 @@ void writeToSocket(asio::ip::tcp::socket& socket, std::string message)
     }
 }
 
+std::vector<std::string> findIP()
+{
+    //create a vector to store the IPs in
+    std::vector<std::string> ipList;
+    //Open a command prompt and use arp -a to retrieve a list of IP addresses and hteir interfaces
+    FILE* pipe = _popen("arp -a", "r");
+    //If the pupe was not created print the error
+    if (!pipe)
+    {
+        fprintf(stderr, "Unable to open IP Scan pipe");
+    }
+    //if the pipe was created continue
+    else
+    {
+        //instantiate buffer and string
+        char buffer[128];
+        std::string bufferString = "";
+
+        //until the pipe contains no more data add the data to the string using the buffer
+        while (!feof(pipe))
+        {
+            if (fgets(buffer, 128, pipe) != NULL)
+            {
+                bufferString += buffer;
+            }
+        }
+        //Close the pipe
+        _pclose(pipe);
+
+        //std::cout << bufferString;
+        //create int for tracking index in string
+        int startIndex = 0;
+        //iterate through bufferString
+        for (int i = 0; i < bufferString.size(); i++)
+        {
+            //if the current value is an integer or a '.' continue
+            if (isdigit(bufferString[i]) || bufferString[i] == '.')
+            {
+                continue;
+            }
+            //if a space is found and the discovered string is of sufficient length to be an IP, add it to the vector
+            else if (bufferString[i] == ' ' && i - startIndex > 3)
+            {
+                ipList.push_back(bufferString.substr(startIndex + 1, i - startIndex - 1));
+            }
+            //set start index to the current index
+            startIndex = i;
+        }
+
+    }
+
+
+    return ipList;
+
+}
+
+//Searches the local subnet/vLAN for IP addresses and attempts to connect to each one
+//Creates a socket and adds it to the socketVector for each connection
+void findAndConnect(std::vector<asio::ip::tcp::socket> &socketVector, asio::io_context &ios)
+{
+    //create a vector to store the IPs in
+    std::vector<std::string> ipList = findIP();
+
+    for (int i = 0; i < ipList.size(); i++)
+    {
+        try
+        {
+            // Create an endpoint designating target server application.
+            // this will be used by the sockets as an access point for communication using TCP
+            std::cout << "Attempting to Connect to " << ipList[i] << "...";
+            asio::ip::tcp::endpoint endpoint(asio::ip::address::from_string(ipList[i]), PORT);
+
+            //Create a socket attached to theendpoint
+            asio::ip::tcp::socket socket(ios, endpoint.protocol());
+
+            //Attempt to connect the socket to the endpoint
+            socket.connect(endpoint);
+
+            std::cout << "Connected" << std::endl;
+        }
+        catch (system::system_error& e) 
+        {
+            std::cout << "Connection Failed " << e.what() << std::endl;
+            //std::cout << "Error occured! Error code = " << e.code()
+            //    << ". Message: " << e.what() << std::endl;;
+
+            //return e.code().value();
+        }
+    }
+
+
+}
 
 
 int main()
 {
-    //NOTE this portion of the code is taken primarily from the following tutorial
-    //this serves as an introduction to boost and its libraries
-    //however all comments are based on my (Grayson) understanding on the code therein and its use
-    //as a part of our overall project
-    //https://subscription.packtpub.com/book/application_development/9781783986545/1/ch01lvl1sec11/creating-an-active-socket
+    //Creates the I/O context to be used as a basis for communicating I/O commands to the OS
+    asio::io_context ios;
 
-    ////Step 1: Create an io_service object
-    ////this provides network I/O services
-    ////it is required for all sockets and is central to our network
-    //asio::io_context ioContext;
-
-    ////Step 2: Create a tcp object
-    ////Acts as a data structure containing values for the TCP protocol
-    ////replace 'v4()' with 'v6()' to change to using IPv6
-    //asio::ip::tcp connectionProtocol = asio::ip::tcp::v4();
-
-    ////Step 3: Create a socket
-    ////Instantiates a socket using our ioService
-    ////This socket is unusable until after it has been opened
-    //asio::ip::tcp::socket socket(ioContext);
-
-    ////Step 3.5: Create error code object
-    ////this will store any error that occurs while opening the socket
-    //boost::system::error_code errorCode;
-
-    ////Step 4: Open the socket
-    ////This will allow the socket to communicate with other sockets
-    //socket.open(connectionProtocol, errorCode);
-
-    ////If an error is received, print said error and return
-    //if (errorCode.value() != 0)
-    //{
-    //    std::cout << "Failed to open socket, Error code = " << errorCode.value() << ". Message " << errorCode.message();
-    //    return errorCode.value();
-    //}
-
-    //Size of queue containing the pending connection requests
-    const int BACKLOG_SIZE = 30;
-
-
-    //Establish the I/O interaction context that allows I/O access
-    asio::io_context ioContext;
-    //Establish Connection Protocol to use
-    asio::ip::tcp connectionProtocol = asio::ip::tcp::v6();
-    //Create an object representing all available IPv6 Addresses available to this program
-    asio::ip::address ipAddress = asio::ip::address_v6::any();
-    //Port we intend to listen with
-    int listenPort = 1998;
-
-
-    //Create an endpoint at our address on our listening port
-    asio::ip::tcp::endpoint endpointServer(ipAddress, listenPort);
-
-
-    //Create an acceptor socket to listen with
-    asio::ip::tcp::acceptor listenSocket(ioContext, connectionProtocol);
-    //Declare error code
-    boost::system::error_code error;
-
-    //Bind the listening endpoint to the acceptor socket
-    listenSocket.bind(endpointServer, error);
-
-    //Sets the acceptor to listen
-    listenSocket.listen(BACKLOG_SIZE);
-
-    //create an active socket for the server to use
-    asio::ip::tcp::socket serverSocket(ioContext);
-
-    if (error.value() != 0)
-    {
-    std::cout << "Failed to open socket, Error code = " << error.value()
-        << ". Message " << error.message();
-    return error.value();
-    }
-
-    //client perspective connection
-    //should target the server's address and port
-    asio::ip::tcp::endpoint endpointClient(ipAddress, listenPort);
-
-    //create an active socket to connect with
-    asio::ip::tcp::socket clientSocket(ioContext, connectionProtocol);
-
-    //connect the socket to the endpoint
-    clientSocket.connect(endpointClient);
-
-    //attempt to accept pending request
-    listenSocket.accept(serverSocket);
-
-    if (error.value() != 0)
-    {
-        std::cout << "Failed to open socket, Error code = " << error.value()
-            << ". Message " << error.message();
-        return error.value();
-    }
-
+    //create vector to hold the active sockets
+    std::vector<asio::ip::tcp::socket> socketVector;
+    
+    findAndConnect(socketVector, ios);
 
 }
