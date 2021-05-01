a6f38b495e196fe271d314c98e480b12bd687d54
Graysire <Graysire@dyrix.net>
2021-04-30 19:53:18 -0700

    Added small listen endpoint, considering refactor for async

--- Group K File Synchronization/Group K File Synchronization.cpp
+++ Group K File Synchronization/Group K File Synchronization.cpp
@@ -124,6 +124,8 @@ int main()
     //create vector to hold the active sockets
     std::vector<asio::ip::tcp::socket> socketVector;
     
+    asyncAccept(listenEndpoint, ios);
+
     findAndConnect(socketVector, ios);
 
 }
